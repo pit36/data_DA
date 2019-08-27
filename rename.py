ff85a93fbe549fb449fa8e0d47c54c28b5219443
pit36 <p.sossalla@googlemail.com>
2019-08-27 12:57:41 +0200

    added meas

---
+++ rename.py
@@ -0,0 +1,12 @@
+import os
+
+dirs = os.listdir()
+dirList= []
+
+for dir in dirs:
+    if os.path.isdir(dir):
+        dirList.append(dir)
+print(dirList)
+#for dir in dirList:
+#    os.rename(dir, str(int(dir)+50))
+
